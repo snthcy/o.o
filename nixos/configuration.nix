a6215f82c0e9098686293528f9195cd99405fbbb
normie <idiot@duck.com>
2023-03-07 14:08:39 -0500

    e

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -121,8 +121,8 @@
         };
       };
     };
+  };
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "22.11";
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  };
 }
